87945e3552dc498eb4f294312c845867986d5f16
SwansonSays <rpswanson18@gmail.com>
2024-08-05 23:47:52 -0700

    integrated sqlite to store messages to display when user reconnect after disconnect

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
+
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );   
+`)
 
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -17,14 +30,37 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+
+    socket.on('chat message', async (msg) => {
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+            //TODO handle the failure
+            console.log(e);
+            return;
+        }
+        io.emit('chat message', msg, result.lastID);
     });
+
+    if (!socket.recovered) {
+        try {
+            await db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            )
+        } catch (e) {
+            console.log(e);
+        }
+    }
 });
 
 
